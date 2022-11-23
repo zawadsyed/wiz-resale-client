8868c2d61aa026bdb8fd7378928d62932eb41519
Syed Zawad <syedzawadd@gmail.com>
2022-11-24 02:30:44 +0600

    firebase authentication in auth provider has created

--- src/AuthProvider/AuthProvider.js
+++ src/AuthProvider/AuthProvider.js
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
-import { getAuth } from 'firebase/auth'
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithPopup, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth'
 import app from '../firebase/firebase.config';
 
 
@@ -7,9 +7,45 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-
-
-    const authInfo = {};
+    // loading state
+    const [loading, setLoading] = useState(true);
+    // user state
+    const [user, setUser] = useState(null);
+    //  Google provider sign in
+    const providerSignIn = provider => {
+        setLoading(true);
+        return signInWithPopup(auth, provider);
+    }
+    // Log in
+    const logIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    // Register
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    //  Log Out
+    const logOut = () => {
+        return signOut(auth);
+    }
+    // For creating user profile photo
+    const updateUserProfile = profile => {
+        return updateProfile(auth.currentUser, profile);
+    }
+    // auth state change observation
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+            console.log('observing current user', currentUser)
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+    const authInfo = { user, createUser, logIn, loading, updateUserProfile, logOut, providerSignIn }
 
     return (
         <div>

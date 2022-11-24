80a39f0f89e1fafd29816982ebfec9534f990d51
Syed Zawad <syedzawadd@gmail.com>
2022-11-25 02:12:37 +0600

    react query installed

--- src/Pages/Home/CategorySection/CategorySection.js
+++ src/Pages/Home/CategorySection/CategorySection.js
@@ -1,6 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const CategorySection = () => {
+    const { data: categories = [] } = useQuery({
+        queryKey: ['categories'],
+        queryFn: () => fetch('')
+            .then(res => res.json())
+    })
+
     return (
         <div>
             <h1>Categories</h1>

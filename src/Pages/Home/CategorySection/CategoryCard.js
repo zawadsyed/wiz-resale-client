d417b37ba90e1cc7d6cf4340dfe777b1529ee355
Syed Zawad <syedzawadd@gmail.com>
2022-11-26 00:37:34 +0600

    categories section added

---
+++ src/Pages/Home/CategorySection/CategoryCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CategoryCard = ({ category }) => {
+    const { category_title, img } = category;
+
+    return (
+        <div className="card bg-base-100 ">
+            <img className='rounded-lg' src={img} alt="car!" />
+            <div className="card-body">
+                <h2 className="text-center text-2xl font-semibold">{category_title}</h2>
+                <Link to={`/categores/${category_title}`}>
+                    <button className="btn btn-primary w-full">Get Now</button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default CategoryCard;
--- src/Pages/Home/CategorySection/CategorySection.js
+++ src/Pages/Home/CategorySection/CategorySection.js
@@ -1,16 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import CategoryCard from './CategoryCard';
 
 const CategorySection = () => {
     const { data: categories = [] } = useQuery({
         queryKey: ['categories'],
-        queryFn: () => fetch('')
+        queryFn: () => fetch('http://localhost:5000/categories')
             .then(res => res.json())
     })
-
+    console.log(categories)
     return (
-        <div>
+        <div className='max-w-[1320px] mx-auto'>
             <h1>Categories</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {
+                    categories.map(category => <CategoryCard key={category._id}
+                        category={category}
+                    ></CategoryCard>)
+                }
+            </div>
 
         </div>
     );
